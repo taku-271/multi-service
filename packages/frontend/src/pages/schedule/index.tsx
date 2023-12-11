a740cb38fb9235f8dbfefb8e0ebe8e67da95b9a0
taku-271 <ta.yamashita.314@gmail.com>
2023-12-12 00:47:36 +0900

    feat: fullCalendarを導入

--- packages/frontend/src/pages/schedule/index.tsx
+++ packages/frontend/src/pages/schedule/index.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import jaLocale from "@fullcalendar/core/locales/ja";
 
 const ScheduleCalendar = () => {
-  const [value, setValue] = useState<Date>();
+  const onEventClick = (dataClickInfo: DateClickArg) => {
+    console.log(dataClickInfo.dateStr);
+  };
 
   return (
     <>
-      <Calendar value={value} onClickDay={(e) => setValue(e)} />
-      <p>{value ? value.toString() : ""}</p>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek listWeek",
+        }}
+        initialView="dayGridMonth"
+        selectable={true}
+        editable={true}
+        dateClick={onEventClick}
+        locales={[jaLocale]}
+        locale="ja"
+      />
     </>
   );
 };
